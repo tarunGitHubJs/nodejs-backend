6b39a7624b9f4da5e26498098a9c0d59de83632c
tarun <tarun15@tradeindia.com>
2024-04-04 16:10:18 +0530

    cors error resolved

--- src/app.js
+++ src/app.js
@@ -4,13 +4,29 @@ import cookieParser from "cookie-parser";
 // const bodyParser = require("body-parser")
 import bodyParser from "body-parser";
 const app = express();
-
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+var allowlist = [
+  // "http://localhost",
+  "http://localhost:8100",
+  // "http://localhost:4400",
+  // "192.168.48.32:3000",
+];
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true, credentials: true };
+  } else {
+    corsOptions = { origin: false, credentials: true };
+  }
+  callback(null, corsOptions);
+};
+app.use(cors(corsOptionsDelegate))
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+//   })
+// );
+// app.options('*', cors())
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
